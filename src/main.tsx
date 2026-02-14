11867496983e4659a5b090796b36d1153c607940
Gustavo Argote <10593140+gooosetavo@users.noreply.github.com>
2026-02-14 12:47:35 -0500

    refactor: add contextproviders to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './contexts/ThemeContext'
+import { WorkspaceProvider } from './contexts/WorkspaceContext'
+import { EditorProvider } from './contexts/EditorContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <WorkspaceProvider>
+        <EditorProvider>
+          <App />
+        </EditorProvider>
+      </WorkspaceProvider>
     </ThemeProvider>
   </StrictMode>,
 )
